2bb1605b79d19ae26cdf47e982ded36a5784c5d6
Matthew Feng <mattfeng@mit.edu>
2018-06-28 18:55:53 -0400

    Mirroring working, updates users and offers indices, users indices have info pulled from both Firebase Auth and Firebase Database

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const algoliasearch = require('algoliasearch')
 
 const firebaseConfig = require('./secrets/firebaseConfig')
 const admin = require('firebase-admin')
-const serviceAccount = require('../secrets/service_account_key.json')
+const serviceAccount = require('./secrets/service_account_key.json')
 
 // configure Firebase admin
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: firebaseConfig.FIREBASE_DB_URL
 })
-const db = firebase.database()
+const db = admin.database()
 
 // configure algolia
 const algolia = algoliasearch(
   algoliaConfig.ALGOLIA_APP_ID,
-  algoliaConfig.ALGOLIA_API_KEY
+  algoliaConfig.ALGOLIA_ADMIN_KEY
 )
 
 const offersIndex = algolia.initIndex(algoliaConfig.ALGOLIA_OFFERS_INDEX)
@@ -24,42 +24,71 @@ const usersIndex = algolia.initIndex(algoliaConfig.ALGOLIA_USERS_INDEX)
 const offersRef = db.ref('offers')
 const usersRef = db.ref('users')
 
-offersRef.on('child_added', addOrUpdateIndexRecord(offersIndex))
-offersRef.on('child_changed', addOrUpdateIndexRecord(offersIndex))
-offersRef.on('child_removed', deleteIndexRecord(offersIndex))
+offersRef.on('child_added', addOrUpdateOfferIndexRecord)
+offersRef.on('child_changed', addOrUpdateOfferIndexRecord)
+offersRef.on('child_removed', deleteOfferIndexRecord)
 
-usersRef.on('child_added', adOrUpdateIndexRecord(usersIndex))
-usersRef.on('child_changed', adOrUpdateIndexRecord(usersIndex))
-usersRef.on('child_removed', deleteIndexRecord(usersIndex))
+usersRef.on('child_added', addOrUpdateUserIndexRecord)
+usersRef.on('child_changed', addOrUpdateUserIndexRecord)
+usersRef.on('child_removed', deleteUserIndexRecord)
 
-function addOrUpdateIndexRecord(index) {
-  return (snapshot) => {
-    const record = snapshot.val()
+function addOrUpdateOfferIndexRecord (snapshot) {
+  const record = snapshot.val()
+  record.objectID = snapshot.key
+  offersIndex
+    .saveObject(record)
+    .then(() => {
+      console.log('Firebase object indexed in Algolia', record.objectID)
+    })
+    .catch(err => {
+      console.error('Error when indexing contact into Algolia', err)
+    })
+}
 
-    record.objectID = snapshot.key
+function deleteOfferIndexRecord (snapshot) {
+  const objectID = snapshot.key
+  offersIndex
+    .deleteObject(objectID)
+    .then(() => {
+      console.log('Firebase object deleted from Algolia', objectID)
+    })
+    .catch(err => {
+      console.error('Error when deleting contact from Algolia', err)
+    })
+}
 
-    index
+function addOrUpdateUserIndexRecord (snapshot) {
+  const record = snapshot.val()
+  record.objectID = snapshot.key
+
+  admin.auth().getUser(snapshot.key).then((details) => {
+    record.displayName = details.displayName
+    record.emailVerified = details.emailVerified
+    record.disabled = details.disabled
+    record.uid = details.uid
+
+    usersIndex
       .saveObject(record)
       .then(() => {
         console.log('Firebase object indexed in Algolia', record.objectID)
       })
       .catch(err => {
-        console.error('Error when indexing contact into Algolia', err)
+        console.error('Error when indexing user into Algolia', err)
       })
-  }
+  }).catch((err) => {
+    console.error('Error when retrieving user info from Firebase Auth', err)
+  })
 }
 
-function deleteIndexRecord(index) {
-  return (snapshot) => {
-    const objectID = snapshot.key
-
-    index
-      .deleteObject(objectID)
-      .then(() => {
-        console.log('Firebase object deleted from Algolia', objectID)
-      })
-      .catch(err => {
-        console.error('Error when deleting contact from Algolia', err)
-      })
-  }
+function deleteUserIndexRecord (snapshot) {
+  const objectID = snapshot.key
+  
+  usersIndex
+    .deleteObject(objectID)
+    .then(() => {
+      console.log('Firebase object deleted from Algolia', objectID)
+    })
+    .catch(err => {
+      console.error('Error when deleting user from Algolia', err)
+    })
 }
